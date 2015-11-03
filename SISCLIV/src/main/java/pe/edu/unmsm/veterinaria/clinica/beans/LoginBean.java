d026f7567c41a915437f58dfcb431adc8f958a41
Cristian1312 <cristianperincio1312@gmail.com>
2015-11-02 23:31:04 -0500

    Se hace una mejora al Login.

--- SISCLIV/src/main/java/pe/edu/unmsm/veterinaria/clinica/beans/LoginBean.java
+++ SISCLIV/src/main/java/pe/edu/unmsm/veterinaria/clinica/beans/LoginBean.java
@@ -7,6 +7,7 @@ package pe.edu.unmsm.veterinaria.clinica.beans;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -14,16 +15,15 @@ import javax.faces.context.FacesContext;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.primefaces.context.RequestContext;
 
 import pe.edu.unmsm.veterinaria.clinica.dao.UsuarioDao;
 import pe.edu.unmsm.veterinaria.clinica.entities.Usuario;
+import pe.edu.unmsm.veterinaria.clinica.interfaces.IUsuarioDao;
 import pe.edu.unmsm.veterinaria.clinica.persistencia.NewHibernateUtil;
 
 /**
  *
  * @author Cristian1312
- * Probando un comentario sin malograr
  */
 @ManagedBean
 @SessionScoped
@@ -34,33 +34,41 @@ public class LoginBean implements Serializable {
 	
     private Usuario usuario;
     
-    public LoginBean() {
-    	this.usuario = new Usuario();
+    @PostConstruct
+    public void init() {
+        this.usuario = new Usuario();
     }
 
-	public void login() {
-        RequestContext context = RequestContext.getCurrentInstance();
+	public String login() {
+		this.session = null;
+        this.transaction = null;
+        Usuario user;
         FacesMessage facesMessage = null;
-        String view = "";
+        int perfil;
+        String view = null;
         
         try {
             this.session = NewHibernateUtil.getSessionFactory().openSession();
-            UsuarioDao usuarioDao = new UsuarioDao();
+            IUsuarioDao usuarioDao = new UsuarioDao();
             this.transaction = this.session.beginTransaction();
-            this.usuario = usuarioDao.verificarUsuario(this.session, this.usuario);
-            if (this.usuario != null) {
-                facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenido",
-                        this.usuario.getUsuario());
-                view = "tareasRecepcionista.xhtml";
+            user = usuarioDao.verificarUsuario(this.session, this.usuario);
+            if (user != null) {
+            	FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuario", user);
+            	perfil = user.getPerfil().getIdPerfil();
+                facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, "Bienvenido", user.getUsuario());
+                switch (perfil) {
+					case 1:
+						view = "tareasMedicoVeterinario?faces-redirect=true";
+						break;
+					case 3:
+						view = "tareasRecepcionista?faces-redirect=true";
+						break;
+				}
             } else {
-                facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login Error",
-                        "Usuario y/o contraseña incorrecto");
-                view = "index.xhtml";
+            	facesMessage = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Login Error",
+                        "Usuario y/o contraseña incorrectos.");
             }
-            this.transaction.commit();
-            
             FacesContext.getCurrentInstance().addMessage(null, facesMessage);
-            context.addCallbackParam("view", view);
         } catch (Exception ex) {
             if (this.transaction != null) {
                 transaction.rollback();
@@ -72,6 +80,8 @@ public class LoginBean implements Serializable {
                 this.session.close();
             }
         }
+        
+        return view;
     }
 	
 	public Usuario getUsuario() {
